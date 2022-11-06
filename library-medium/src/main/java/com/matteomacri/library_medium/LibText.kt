8982a27f7e78924a093e0a2411c0081ed878b9c8
matteomacri18 <matteomacri18@gmail.com>
2022-11-06 15:00:06 +0100

    init

---
+++ library-medium/src/main/java/com/matteomacri/library_medium/LibText.kt
@@ -0,0 +1,9 @@
+package com.matteomacri.library_medium
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomText(){
+    Text("This is my library")
+}
